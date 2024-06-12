7b6379220a18bdd428e7dc98d921246bc02a5af1
hicham-oualla <hich2022am.oualla@gmail.com>
2024-06-12 15:52:37 +0100

    creation du classe film

---
+++ S-moviesapp/src/main/java/com/project/moviesapp/model/Film.java
@@ -0,0 +1,30 @@
+package com.project.moviesapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Film {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id_film;
+        private String text ;
+        private  String description ;
+        private Date date_debut;
+        private  Date date_fin ;
+        private String saison ;
+        private String genre ;
+        private String createur ;
+        private String acteur ;
+        private String note ;
+
+
+    }
