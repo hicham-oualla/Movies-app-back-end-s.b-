6c31e47eb7852791a130a3360255d74b1603ce43
hicham-oualla <hich2022am.oualla@gmail.com>
2024-06-13 11:15:05 +0100

    creation du interface film repository

--- S-moviesapp/src/main/java/com/project/moviesapp/repository/filmRepository.java
+++ S-moviesapp/src/main/java/com/project/moviesapp/repository/filmRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface userRepository extends CrudRepository<Film, Integer> {
+public interface filmRepository extends CrudRepository<Film, Integer> {
 }
