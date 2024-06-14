83483af2d654e874e288fa0637fec52a673d08e4
hicham-oualla <hich2022am.oualla@gmail.com>
2024-06-14 10:27:02 +0100

    creation du interface serie service

---
+++ S-moviesapp/src/main/java/com/project/moviesapp/service/SeriesService.java
@@ -0,0 +1,14 @@
+package com.project.moviesapp.service;
+
+
+
+import com.project.moviesapp.model.Series;
+
+import java.util.List;
+
+public interface SeriesService {
+    Series saveseries(Series series);
+    List<Series> getAllseries();
+    Series getFilmById(int id);
+
+}
